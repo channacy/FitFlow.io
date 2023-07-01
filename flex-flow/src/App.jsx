43ffac5d9227a3c71deaf551d7e9e4c445f87e3c
nxya <nathacutlan@gmail.com>
2023-07-01 17:19:09 -0400

    addded camera

--- flex-flow/src/App.jsx
+++ flex-flow/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import Input from './Components/Input';
-
-
-
+import { useState, useEffect } from 'react'
+import Input from './components/Input';
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = "/src/main.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
 
   return (
     <div className="App">
-      
       <div>
         <h1 className="header">Fit-Flow</h1>
         <div className="inputContainer">
@@ -18,15 +26,12 @@ const App = () => {
           <Input />
           <Input />
           <button id="startBtn">Start Exercise</button>
-        </div>
-  
-
+        </div>	
+	<video className="input_video"></video>
+	<canvas className="output_canvas" width="1280px" height="720px"></canvas>
+	<div className="landmark-grid-container"></div>
       </div>
-      
-   
-     
-
-      
+      <script src="/src/main.js"></script>
     </div>
   )
 }
