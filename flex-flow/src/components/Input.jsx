9029ec8f061c6017f445e041f9f48b9150f03c33
channacy <68759286+channacy@users.noreply.github.com>
2023-07-01 18:20:53 -0400

    menu items prop added

--- flex-flow/src/components/Input.jsx
+++ flex-flow/src/components/Input.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import PropTypes from 'prop-types';
 
 const Input = (props) => {
-  const { label } = props;
+  const { label, menuItems = []} = props;
   const [age, setAge] = React.useState('');
 
   const handleChange = (event) => {
@@ -26,9 +27,11 @@ const Input = (props) => {
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {menuItems && menuItems.map((item) => (
+            <MenuItem key={item.value} value={item.value}>
+              {item.label}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
     </div>
@@ -36,3 +39,14 @@ const Input = (props) => {
 };
 
 export default Input;
+
+Input.propTypes = {
+    label: PropTypes.string.isRequired,
+    menuItems: PropTypes.arrayOf(
+      PropTypes.shape({
+        value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        label: PropTypes.string,
+      })
+    ).isRequired,
+  };
+  
